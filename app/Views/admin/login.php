078605fc3b6aa3c24d4619a6fce9958b2ebc3c9f
Osamu95 <jonathanandriamalaza@gmail.com>
2026-07-20 17:04:04 +0300

    place older values

--- app/Views/admin/login.php
+++ app/Views/admin/login.php
@@ -11,8 +11,8 @@
                 <?php endif; ?>
                 <form action="<?= base_url('admin/login') ?>" method="post" class="card p-4 shadow">
                     <h3 class="mb-3">Espace Admin</h3>
-                    <input type="email" name="email" class="form-control mb-3" placeholder="Email" required>
-                    <input type="password" name="mdp" class="form-control mb-3" placeholder="Mot de passe" required>
+                    <input type="email" name="email" class="form-control mb-3" value="admin@mvola.com" placeholder="Email" required>
+                    <input type="password" name="mdp" class="form-control mb-3" value="123456" placeholder="Mot de passe" required>
                     <button type="submit" class="btn btn-success w-100">Connexion</button>
                 </form>
             </div>
