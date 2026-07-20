a92d8b5b6bb41f1606a75375ed7d37c86255ea63
Osamu95 <jonathanandriamalaza@gmail.com>
2026-07-20 15:01:50 +0300

    accueil ver operateur ou client

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,7 +5,7 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-//$routes->get('/', 'Client\Auth::index');
+$routes->get('/', 'Home::index');
 
 
 $routes->group('client', function($routes) { 
