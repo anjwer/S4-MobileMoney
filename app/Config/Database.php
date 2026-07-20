fc9c147118a29e81cd7b31e01e41223a0f938cae
anjara <loranasl17@gmail.com>
2026-07-20 09:54:43 +0300

    script base

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -59,7 +59,7 @@ class Database extends Config
      * @var array<string, mixed>
      */
     public array $default = [
-        'database'    => 'database.db',
+        'database'    => 'WRITEPATH/database.sqlite',
         'DBDriver'    => 'SQLite3',
         'DBPrefix'    => '',
         'DBDebug'     => true,
